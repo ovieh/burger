06c25c3bf901153021a0a2a96c91d703829f5389
ovieh <ovieh.mosley@gmail.com>
2017-11-21 12:42:33 -0500

    can update column

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,6 +1,23 @@
 $(function () {
 
+    $(".change-devour").on("click", function (event)  {
+        const id = $(this).data("id");
+        const newDevour = $(this).data("newdevour");
 
+        const newDevourState = {
+            devoured: newDevour
+        }
+
+        $.ajax("/api/burgers/" + id, {
+            type: "PUT",
+            data: newDevourState
+        })
+            .then( () =>{
+                console.log("changed devoured to", newDevour);
+
+                location.reload();
+            })
+    });
 
     $('.create-form').on('submit', event => {
         event.preventDefault();
